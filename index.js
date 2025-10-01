717084a1fdbfddca83df16a4f872d57bc588b087
Maskedup001 <ilya-alekseev-2006@list.ru>
2025-10-01 11:42:23 +0300

    lesson3

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log(1);
+// Первый урок 1
+// console.log(1)
+// Второй урок 2
+const age = 35;
+if (age < 18) {
+    console.log('Доступ запрещён');
+}
+else {
+    console.log(`Таблица умножения на ${age}:`);
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${age} × ${i} = ${age * i}`);
+    }
+}
--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log(1)
+// Первый урок 1
+// console.log(1)
+
+// Второй урок 2
+const age: number = 35;
+
+if (age < 18) {
+console.log('Доступ запрещён');
+} 
+else {
+    console.log(`Таблица умножения на ${age}:`);
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${age} × ${i} = ${age * i}`);
+    }
+}
