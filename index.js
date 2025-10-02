8033573a72af2821842f22eabdc55c351d94ecd9
Maskedup001 <ilya-alekseev-2006@list.ru>
2025-10-02 11:10:34 +0300

    lesson4

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 // Первый урок 1
 // console.log(1)
 // Второй урок 2
-const age = 35;
-if (age < 18) {
-    console.log('Доступ запрещён');
+// const age: number = 35;
+// if (age < 18) {
+// console.log('Доступ запрещён');
+// } 
+// else {
+//     console.log(`Таблица умножения на ${age}:`);
+//     for (let i = 1; i <= 10; i++) {
+//         console.log(`${age} × ${i} = ${age * i}`);
+//     }
+// }
+// Третий урок 3
+// Функция возвращает число или строку (тип number или string)
+function calculate2(a, b, operator) {
+    if (operator === "+")
+        return a + b;
+    if (operator === "-")
+        return a - b;
+    if (operator === "*")
+        return a * b;
+    if (operator === "/")
+        return a / b;
+    // Если оператор не распознан, возвращается строка с ошибкой
+    return "Ошибка: неизвестный оператор";
 }
-else {
-    console.log(`Таблица умножения на ${age}:`);
-    for (let i = 1; i <= 10; i++) {
-        console.log(`${age} × ${i} = ${age * i}`);
-    }
-}
+//Вывод  
+console.log("Результат:", calculate2(10, 5, "+"));
+console.log("Результат:", calculate2(10, 5, "-"));
+console.log("Результат:", calculate2(10, 5, "*"));
+console.log("Результат:", calculate2(10, 5, "/"));
+// Пример, если оператор не распознан (выведет строку с ошибкой)
+console.log("Результат:", calculate2(10, 5, "$wd"));
