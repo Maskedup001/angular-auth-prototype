89756bf33ee10774d77dcf68488903d0b5b2e14a
Maskedup001 <ilya-alekseev-2006@list.ru>
2025-10-06 11:10:24 +0300

    lesson6

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
 // Первый урок 
-// Массив товаров 
-const products = [
-    { name: "Телефон", price: 500, inStock: true },
-    { name: "Ноутбук", price: 1000, inStock: false },
-    { name: "Наушники", price: 100, inStock: true },
-    { name: "Мышь", price: 50, inStock: true },
-    { name: "Клавиатура", price: 80, inStock: false },
-];
-//Только товары в наличии(на складе)
-const A_Products = products.filter(product => product.inStock);
-// Массив цен этих товаров.
-const A_Prices = A_Products.map(product => product.price);
-// Общая сумма товаров в наличии
-let total = 0;
-for (let i = 0; i < A_Products.length; i++) {
-    total += A_Products[i].price;
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+// Главная функция 
+function showUserId() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const data = yield fetch('https://jsonplaceholder.typicode.com/posts');
+        const posts = yield data.json();
+        // Массив
+        const result = [];
+        // Перебор всех постов 
+        for (let post of posts) {
+            let exists = false;
+            // Проверка всех ID которые уже есть в результате
+            for (let id of result) {
+                if (id === post.userId) {
+                    exists = true;
+                    break;
+                }
+            }
+            // Если такого ID еще нет в результате то добавляем
+            if (!exists) {
+                result.push(post.userId);
+            }
+        }
+        // Сортировка ID по возрастанию
+        result.sort((a, b) => a - b);
+        console.log('Список всех userId:');
+        for (let id of result) {
+            console.log(id);
+        }
+    });
 }
-// Сортировка товаров в наличии по цене (от дешёвых к дорогим)
-const Sorted_Products = [...A_Products].sort((a, b) => a.price - b.price);
-// Вывод
-console.log("Товары в наличии:", A_Products);
-console.log("Цены товаров в наличии:", A_Prices);
-console.log("Общая сумма:", total);
-console.log("Отсортированные товары:", Sorted_Products);
+showUserId();
--- index.ts
+++ index.ts
@@ -47,41 +47,88 @@
 // console.log("Результат:", calculate2(10, 5, "$wd"));
 
 
-// Пятый урок 5
+// // Пятый урок 5
 
-// Тип Product
-type Product = {
-	name: string;
-	price: number;
-	inStock: boolean;
+// // Тип Product
+// type Product = {
+// 	name: string;
+// 	price: number;
+// 	inStock: boolean;
+// }
+
+// // Массив товаров 
+// const products: Product[] = [
+// 	{name:"Телефон", price:500, inStock:true},
+// 	{name:"Ноутбук", price:1000, inStock:false},
+// 	{name:"Наушники", price:100, inStock:true},
+// 	{name:"Мышь", price:50, inStock:true},
+// 	{name:"Клавиатура", price:80, inStock:false},
+// ]
+
+// //Только товары в наличии(на складе)
+// const A_Products = products.filter(product => product.inStock);
+
+// // Массив цен этих товаров.
+// const A_Prices = A_Products.map(product => product.price);
+
+// // Общая сумма товаров в наличии
+// let total = 0;
+// for(let i = 0; i < A_Products.length; i++) {
+// 		total += A_Products[i].price;
+// }
+
+// // Сортировка товаров в наличии по цене (от дешёвых к дорогим)
+// const Sorted_Products = [...A_Products].sort((a,b) => a.price - b.price);
+
+// // Вывод
+// console.log("Товары в наличии:", A_Products);
+// console.log("Цены товаров в наличии:", A_Prices);
+// console.log("Общая сумма:", total);
+// console.log("Отсортированные товары:", Sorted_Products)
+
+// Урок 6
+
+// Описание как должны выглядеть данные о посте с сервера
+interface Post {
+  userId: number;  
+  id: number;      
+  title: string;   
+  body: string;    
 }
 
-// Массив товаров 
-const products: Product[] = [
-	{name:"Телефон", price:500, inStock:true},
-	{name:"Ноутбук", price:1000, inStock:false},
-	{name:"Наушники", price:100, inStock:true},
-	{name:"Мышь", price:50, inStock:true},
-	{name:"Клавиатура", price:80, inStock:false},
-]
-
-//Только товары в наличии(на складе)
-const A_Products = products.filter(product => product.inStock);
-
-// Массив цен этих товаров.
-const A_Prices = A_Products.map(product => product.price);
-
-// Общая сумма товаров в наличии
-let total = 0;
-for(let i = 0; i < A_Products.length; i++) {
-		total += A_Products[i].price;
+// Главная функция 
+async function showUserId() {
+  const data = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const posts: Post[] = await data.json();
+  
+  // Массив
+  const result: number[] = [];
+  
+  // Перебор всех постов 
+  for (let post of posts) {
+    let exists = false; 
+    
+    // Проверка всех ID которые уже есть в результате
+    for (let id of result) {
+      if (id === post.userId) {
+        exists = true;
+        break; 
+      }
+    }
+    
+    // Если такого ID еще нет в результате то добавляем
+    if (!exists) {
+      result.push(post.userId);
+    }
+  }
+  
+  // Сортировка ID по возрастанию
+  result.sort((a, b) => a - b);
+  
+  console.log('Список всех userId:');
+  for (let id of result) {
+    console.log(id);
+  }
 }
 
-// Сортировка товаров в наличии по цене (от дешёвых к дорогим)
-const Sorted_Products = [...A_Products].sort((a,b) => a.price - b.price);
-
-// Вывод
-console.log("Товары в наличии:", A_Products);
-console.log("Цены товаров в наличии:", A_Prices);
-console.log("Общая сумма:", total);
-console.log("Отсортированные товары:", Sorted_Products)
+showUserId();
